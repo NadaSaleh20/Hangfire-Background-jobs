900cff6fd409e08b35d42914c94f944733fbd5e8
NadaSaleh20 <nadasaleh987@gmail.com>
2023-03-23 22:22:31 +0200

    Add project files.

---
+++ BackGroundHangFire/Controllers/WeatherForecastController.cs
@@ -0,0 +1,57 @@
+﻿using Google.Api.Ads.AdWords.v201809;
+using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackGroundHangFire.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            //var batchId = BatchJob.StartNew(x =>
+            //{
+            //    x.Enqueue(() => Console.WriteLine("Job 1"));
+            //    x.Enqueue(() => Console.WriteLine("Job 2"));
+            //});
+   
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public void GetMessage()
+        {
+            Console.WriteLine($"I'm The Parent {DateTime.Now}");
+        }
+
+
+    }
+}
